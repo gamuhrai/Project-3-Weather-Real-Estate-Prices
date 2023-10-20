9b6d4c568f15493c42d2585cb2c6e1dbbf30e37b
ZiyeJin <135785800+JJJJ0220@users.noreply.github.com>
2023-10-19 21:04:37 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,64 @@
-from flask import Flask, render_template
-import pandas as pd
-import sqlite3
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import plotly
+import plotly.graph_objs as go
+import json
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_BINDS'] = {
+    'homevalue': 'sqlite:///home_data.db',
+    'temperature': 'sqlite:///temperature_data.db'
+}
+db = SQLAlchemy(app)
+
+class HomeValue(db.Model):
+    __bind_key__ = 'homevalue'
+    __tablename__ = 'home_data'  # Added the correct table name here
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    value = db.Column(db.Float, nullable=False)
+
+class Temperature(db.Model):
+    __bind_key__ = 'temperature'
+    __tablename__ = 'temperature_data'  # Added the correct table name here
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    temperature = db.Column(db.Float, nullable=False)
+
+with app.app_context():
+    engine_home = db.engines['homevalue']
+    HomeValue.__table__.create(engine_home, checkfirst=True)  # Added checkfirst=True here for safety
+    
+    engine_temp = db.engines['temperature']
+    Temperature.__table__.create(engine_temp, checkfirst=True)  # Added checkfirst=True here for safety
 
 @app.route('/')
 def index():
-    # Connect to the SQLite database
-    conn = sqlite3.connect("final_temp.db")
+    return render_template('index_updated.html')
 
-    # Query the desired table and read it into a pandas DataFrame
-    df = pd.read_sql_query('SELECT city, "2018-10-31", "2018-11-30", "2018-12-31", "2019-01-31", "2019-02-28", "2019-03-31", "2019-04-30", "2019-05-31", "2019-06-30", "2019-07-31", "2019-08-31", "2019-09-30", "2019-10-31", "2019-11-30", "2019-12-31", "2020-01-31", "2020-02-29", "2020-03-31", "2020-04-30", "2020-05-31", "2020-06-30", "2020-07-31", "2020-08-31", "2020-09-30", "2020-10-31", "2020-11-30", "2020-12-31", "2021-01-31", "2021-02-28", "2021-03-31", "2021-04-30", "2021-05-31", "2021-06-30", "2021-07-31", "2021-08-31", "2021-09-30", "2021-10-31", "2021-11-30", "2021-12-31", "2022-01-31", "2022-02-28", "2022-03-31", "2022-04-30", "2022-05-31", "2022-06-30", "2022-07-31", "2022-08-31", "2022-09-30", "2022-10-31", "2022-11-30", "2022-12-31", "2023-01-31", "2023-02-28", "2023-03-31", "2023-04-30", "2023-05-31", "2023-06-30", "2023-07-31", "2023-08-31", "2023-09-30" FROM "final_temp"', conn)
+@app.route('/data/home_value/<city>')
+def get_home_value(city):
+    data = HomeValue.query.filter_by(city=city).all()
+    years = [x.year for x in data]
+    values = [x.value for x in data]
+    graph = {
+        'data': [{'x': years, 'y': values, 'type': 'line', 'name': city}],
+        'layout': {'title': f'Home Value for 1 bedroom over the past 5 years in {city}'}
+    }
+    return json.dumps([graph], cls=plotly.utils.PlotlyJSONEncoder)
 
-    # Close the connection
-    conn.close()
-
-    # Convert the DataFrame to a list of dictionaries for easier rendering in the template
-    data = df.to_dict(orient='records')
-
-    return render_template('index.html', data=data)
+@app.route('/data/temperature/<city>')
+def get_temperature(city):
+    data = Temperature.query.filter_by(city=city).all()
+    years = [x.year for x in data]
+    temps = [x.temperature for x in data]
+    graph = {
+        'data': [{'x': years, 'y': temps, 'type': 'line', 'name': city}],
+        'layout': {'title': f'Temperature over the past 5 years in {city}'}
+    }
+    return json.dumps([graph], cls=plotly.utils.PlotlyJSONEncoder)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
